4e687f88e314603ca0c0c7f211f8e8b1a655602e
wanghui <wanghui@timeface.cn>
2017-07-03 16:33:08 +0800

    mark

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,10 +18,14 @@ import dao.ItemsCustomMapper;
 import dao.base.ItemsMapper;
 import entity.ItemsCustom;
 import entity.base.Items;
+import service.ItemsService;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloController extends BaseController {
+	
+	private static Logger logger = LoggerFactory.getLogger(HelloController.class);
+	
 	@Autowired
 	ItemsMapper itemsMapper;
 	
@@ -28,11 +35,29 @@ public class HelloController extends BaseController {
 	@Autowired
 	ItemsCustomMapper iMapper;
 	
+	@Autowired
+	ItemsService itemsService;
+	
 	@RequestMapping("/helloWorld")
 	public @ResponseBody Items helloWorld(HttpServletRequest request) throws Exception{
+		logger.info("123456");
 		System.out.println(request.getParameter("name"));
-		Items items = itemsMapper.selectByPrimaryKey(1);
+		// Items items = itemsMapper.selectByPrimaryKey(1);
+		Items items = itemsService.findById(1);
 		return items;
+	}
+	
+	@RequestMapping("/update")
+	public @ResponseBody String updateTx(HttpServletRequest request) {
+		try {
+			String id = request.getParameter("id");
+			String name = request.getParameter("name");
+			itemsService.update(id, name);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "fail";
+		}
+		return "success";
 	}
 	
 //	@RequestMapping(value="/onetoone",method={RequestMethod.POST,RequestMethod.GET})
